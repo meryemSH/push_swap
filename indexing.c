42419f98db2e59fc7a5497afbfbcae2f42864aaa
Meryem Seghrouchni Idrissi <mseghrou@e1r11p12.1337.ma>
2025-12-25 09:29:50 +0100

    push

--- indexing.c
+++ indexing.c
@@ -6,58 +6,41 @@
 /*   By: mseghrou <mseghrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 11:42:28 by mseghrou          #+#    #+#             */
-/*   Updated: 2025/12/25 09:13:56 by mseghrou         ###   ########.fr       */
+/*   Updated: 2025/12/25 09:28:53 by mseghrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "push_swap.h"
 
-int	get_min_not_indexed(t_stack *stack)
+t_stack	*get_min(t_stack *stack)
 {
-	int	min;
-	int	found;
+	t_stack	*min;
 
-	found = 0;
+	min = NULL;
 	while (stack)
 	{
 		if (stack->index == -1)
 		{
-			if (!found || stack->data < min)
-			{
-				min = stack->data;
-				found = 1;
-			}
+			if (!min || stack->data < min->data)
+				min = stack;
 		}
 		stack = stack->next;
-		
 	}
 	return (min);
 }
-
 void	assign_index(t_stack *stack)
 {
-	int		index;
-	int		min;
-	t_stack	*tmp;
+	t_stack	*min;
+	int		i;
 
-	index = 0;
+	i = 0;
 	while (1)
 	{
-		tmp = stack;
-		min = get_min_not_indexed(tmp);
-
-		while (tmp)
-		{
-			if (tmp->data == min && tmp->index == -1)
-			{
-				tmp->index = index;
-				index++;
-				break ;
-			}
-			tmp = tmp->next;
-		}
-		if (!tmp)
+		min = get_min(stack);
+		if (!min)
 			break ;
+		min->index = i;
+		i++;
 	}
 }
